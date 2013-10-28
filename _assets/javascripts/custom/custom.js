8ebf6632b49face0ebc7b33d12daa86cbab1fd62
AJ Acevedo <aj@ajalabs.com>
2013-10-28 03:13:51 -0400

    The asset pipeline is now locked and loaded!
    - Custom assets go into: assets/{asset_type}/custom
    - Moved all assets in vendor/assets into assets/...
    - Removed vendor from the asset pipeline
    - Added Bootstrap Sass 3.0.0
    - Added Font Awesome 4.0.1
    - Font icons are added to the asset pipeline via asset_path()
    - Renamed application.scss -> jekyllkb.scss
    - Renamed application.js -> jekyllrb.js

---
+++ _assets/javascripts/custom/custom.js
@@ -0,0 +1,4 @@
+/**
+* Custom Javascripts
+* If you prefer to use CoffeeScript, rename this files to custom.coffee
+*/
--- _assets/javascripts/jekyllkb.js
+++ _assets/javascripts/jekyllkb.js
@@ -1,2 +1,3 @@
 //= require jquery
 //= require bootstrap
+//= require modernizr
